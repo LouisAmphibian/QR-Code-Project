12c3baccd6f48987184eb011bf9a3839876d2da2
LouisAmphibian <ayandamantshinga30@gmail.com>
2024-09-19 21:08:20 +0200

    The project can now create QR code

--- QR Code Project/index.js
+++ QR Code Project/index.js
@@ -1,5 +1,39 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+import inquirer from 'inquirer'; //import the npm
+import qr from "qr-image";
+import fs from "fs";
+
+/*2 use the qr-image npm package to turn the user URL into a QR code image*/
+//var qr = require('qr-image');
+
+/*1 THE Inquirer npm package to get user input*/
+inquirer
+.prompt([
+    /* Pass questions */
+    {
+    message: "Type in your URL",
+    name:"URL",
+    },
+  ])
+  .then((answers) => {
+    // feedback 
+    const url = answers.URL;
+
+    // Generate QR code
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    // Save URL to file
+    fs.writeFile('URL.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.error('Prompt could not be rendered in this environment.');
+    } else {
+      // Something else went wrong
+      console.error('Something else went wrong:', error);
+    }
+  });
